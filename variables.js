941462d424832c3e8261d2f3e89c6e65545b7b6b
Ian Nordby <ian.c.nordby@gmail.com>
2022-09-20 15:00:03 -0600

    Add variable reassignmnet

--- variables.js
+++ variables.js
@@ -1,6 +1,9 @@
 var hello = 'Hello World'
 var name = "Ian"
 var animal = "turtle"
+hello = "goodbye"
+name = "Ron Mexico"
+animal = "tiger"
 
 var year = 2022
 var states = 50
@@ -9,3 +12,7 @@ var fingers = 4 //dang...
 var truth = false
 var hungry = true
 var rich = true
+
+console.log(hello, name, animal)
+console.log(year, states, fingers)
+console.log(truth, hungry, rich)
